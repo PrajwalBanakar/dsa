9db0f6462c99216b881495f108289cdca032d54b
Prajwal Banakar <prajwalbanakar040999@gmail.com>
2023-09-20 22:03:58 -0400

    binary search

--- src/main/java/com/pb/App.java
+++ src/main/java/com/pb/App.java
@@ -8,13 +8,18 @@ public class App
 {
     public static void main( String[] args )
     {
-        minStack minStack = new minStack();
-        minStack.push(-2);
-        minStack.push(0);
-        minStack.push(-3);
-        System.out.println(minStack.getMin()); // return -3
-        minStack.pop();
-       System.out.println(minStack.top());     // return 0
-       System.out.println(minStack.getMin()); // return -2
+    //     minStack minStack = new minStack();
+    //     minStack.push(-2);
+    //     minStack.push(0);
+    //     minStack.push(-3);
+    //     System.out.println(minStack.getMin()); // return -3
+    //     minStack.pop();
+    //    System.out.println(minStack.top());     // return 0
+    //    System.out.println(minStack.getMin()); // return -2
+
+    binarySearch bSearch = new binarySearch();
+    int[] nums = {-1,0,3,5,9,12};
+    int index = bSearch.search(nums, 9);
+    System.out.println(index);
     }
 }
